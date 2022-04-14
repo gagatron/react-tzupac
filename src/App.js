e1164ba17c120f9c409fbb720000515b695de4a3
gagatron <adam_siwek@msn.com>
2022-04-14 13:14:30 -0400

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./style.css";
 import  tt from "./test.css" ;
-import log1 from './apples.jpg';
+import log1 from "./apples.jpg";
 
 const divStyle = {
   paddingLeft: '10%',
@@ -21,20 +21,22 @@ export default function App() {
     <div style={divStyle}>
       <h1 style={divStyle}>Hello StackBlitz!</h1>
       <div style={divStyle}> <h1 style={divStyle}>adam xxxx </h1>
-      you are incredible asdfjasdfjasdfj;
+      you are incredible
       lasdjf;lakdjf;lajsdfl
       ;jasl;dfjjadfjalsdjflajdfjdfjlasdlfkjas 
       ldfkjadfjjalsdfkjaldfkjjasdlfkj
       adslkfjalsdkjflasd
-      kjfladkjfldjkflkadjflaskdjfalsdk
+      kjfladkjfldjkf lkadjflaskdjfalsdk
       jfladksjfladskjflasdkjfladsj
       flasdjkfljjdj dfjjadfjalsdjflajdfjdfjl
       asdlfkjasldfkjadfjjalsd 
-      fkjaldfkjjasdlf kjadslkfjalsdkjflasdkjfladkjfldj kflkadjflaskdjfalsdkjfladksjf
+      fkjaldfkjjasdlf kjadslkfjalsdkj
+      flasdkjfladkjfldj kflkadjflaskdjfalsd
+      kjfladksjf
       ladskjflasdkjfladsjflasdjkfljjdj</div>
       <p style={divStyle} >Start editing to see some magic happen :)</p>
       <div>
-        <img src={log1} />
+      <img src={'./apples.jpg'} width={400} height={500}/>
         </div>
 
       </div>
