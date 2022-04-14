bc69b029ddb03354cae1452c0e8c94f3ce17049a
gagatron <adam_siwek@msn.com>
2022-04-14 10:55:13 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-tzupac
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-tzupac)
---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./style.css";
+import  tt from "./test.css" ;
+
+const divStyle = {
+  paddingLeft: '10%',
+  paddingRight: '10%',
+  backgroundColor : 'blue',
+  textAlign: 'center',
+  margin: '0%',
+  fontFamily: 'Arial',
+  display: 'flex'
+  }
+   
+
+  
+
+export default function App() {
+  return (
+    <div style={divStyle}>
+      <h1 style={divStyle}>Hello StackBlitz!</h1>
+      <div style={divStyle}> <h1 style={divStyle}>adam xxxx </h1>
+      you are incredible asdfjasdfjasdfj;
+      lasdjf;lakdjf;lajsdfl
+      ;jasl;dfjjadfjalsdjflajdfjdfjlasdlfkjas 
+      ldfkjadfjjalsdfkjaldfkjjasdlfkj
+      adslkfjalsdkjflasd
+      kjfladkjfldjkflkadjflaskdjfalsdk
+      jfladksjfladskjflasdkjfladsj
+      flasdjkfljjdj dfjjadfjalsdjflajdfjdfjl
+      asdlfkjasldfkjadfjjalsd 
+      fkjaldfkjjasdlf kjadslkfjalsdkjflasdkjfladkjfldj kflkadjflaskdjfalsdkjfladksjf
+      ladskjflasdkjfladsjflasdjkfljjdj</div>
+      <p style={divStyle} >Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
